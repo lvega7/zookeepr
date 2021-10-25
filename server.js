70a7e72b60d54eeee36fd7f59abcaf93387e62a1
Lissette Vega <lissettevega@Lissettes-MacBook-Pro.local>
2021-10-25 12:18:56 -0400

    Add more Heroku

--- server.js
+++ server.js
@@ -45,6 +45,11 @@ function filterByQuery(query, animalsArray){
     return filteredResults;
 }
 
+function findById(id, animalsArray){
+    const results = animalsArray.filter(animal => animal.id === id)[0];
+    return results;
+}
+
 
 app.get ('/api/animals', (req, res) => {
     let results = animals;
@@ -52,6 +57,15 @@ app.get ('/api/animals', (req, res) => {
         results = filterByQuery(req.query, results);
     }
     res.json(results);
+});
+app.get('/api/animals/:id', (req,res) => {
+    const results = findById(req.params.id, animals);
+    if(result){
+        res.json(result);
+    }
+    else {
+        res.send(404);
+    }
 });
 
 app.listen(PORT, () => {
